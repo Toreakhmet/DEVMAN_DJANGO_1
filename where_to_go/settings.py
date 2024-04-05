feefe6391ea0136d9e20a9d744b4bdb574966fec
Toreakhmet <s.vasilovich@bk.ru>
2024-04-06 01:46:11 +0500

    Update settings.py

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -97,6 +97,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))  # Set this to your path
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'  # URL to serve media files
